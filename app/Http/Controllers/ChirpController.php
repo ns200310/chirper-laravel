ccd746dd3ca4d8c17b6eb95c31d6a56f72f0add0
NITHIKA SENAWEERA <2206290@uad.ac.uk>
2024-12-27 00:34:26 +0000

    feat: Sample testing on the chirp controller

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -5,15 +5,16 @@ namespace App\Http\Controllers;
 use App\Models\Chirp;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class ChirpController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index() : Response
     {
-        //
+        return response('Hello world');
     }
 
     /**
