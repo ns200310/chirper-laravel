32c0a9751a7dc1517fa8e0ba5986446976ad74d1
NITHIKA SENAWEERA <2206290@uad.ac.uk>
2025-01-03 03:43:11 +0000

    feat: Created a custom notification to handle chirps

--- app/Notifications/NewChirp.php
+++ app/Notifications/NewChirp.php
@@ -2,7 +2,9 @@
 
 namespace App\Notifications;
 
+use App\Models\Chirp;
 use Illuminate\Bus\Queueable;
+use Illuminate\Support\Str;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
@@ -14,9 +16,9 @@ class NewChirp extends Notification
     /**
      * Create a new notification instance.
      */
-    public function __construct()
+    public function __construct(public Chirp $chirp)
     {
-        //
+        
     }
 
     /**
@@ -35,9 +37,12 @@ class NewChirp extends Notification
     public function toMail(object $notifiable): MailMessage
     {
         return (new MailMessage)
-                    ->line('The introduction to the notification.')
-                    ->action('Notification Action', url('/'))
-                    ->line('Thank you for using our application!');
+        ->subject("New Chirp from {$this->chirp->user->name}")
+        ->greeting("New Chirp from {$this->chirp->user->name}")
+        ->line(Str::limit($this->chirp->message, 50))
+        ->action('Go to Chirper', url('/'))
+        ->line('Thank you for using our application!');
+     
     }
 
     /**
